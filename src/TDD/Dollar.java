36b1690cc62915686ab7cbbae1f144e9fae40edb
Noriaki Koyanagi <koyanagn@gmail.com>
2012-10-28 15:37:03 +0900

    Part3 P16

--- src/TDD/Dollar.java
+++ src/TDD/Dollar.java
@@ -10,4 +10,7 @@ class Dollar {
 	Dollar times(int multiplier){
 		return new Dollar(amount * multiplier);
 	}
+	public boolean equals(Object object){
+		return true;
+	}
 }
--- src/TDD/DollarTest.java
+++ src/TDD/DollarTest.java
@@ -14,5 +14,9 @@ public class DollarTest {
 		product = five.times(3);
 		assertEquals(15, product.amount);
 	}
+	@Test
+	public void testEquality(){
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+	}
 
 }
